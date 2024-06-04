480669f0e1aef9a25750482536459c0c87f4799e
fenec_fox <atimevil@gmail.com>
2024-06-04 20:36:49 +0900

    init

---
+++ src/main/java/com/sparta/icy/IcyApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.icy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/icy/IcyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.icy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
