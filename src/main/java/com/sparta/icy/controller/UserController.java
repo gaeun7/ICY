f849e4f7a93e9bfa99b6b07b399b490d5d74a935
이도 <skydoya191539@naver.com>
2024-06-07 14:10:20 +0900

    login 기능 구현

---
+++ src/main/java/com/sparta/icy/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.sparta.icy.controller;
+
+import com.sparta.icy.dto.LoginRequestDto;
+import com.sparta.icy.dto.SignupRequestDto;
+import com.sparta.icy.dto.LoginRequestDto;
+import com.sparta.icy.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping("user/signup")
+    public String signup(SignupRequestDto requestDto) {
+        userService.signup(requestDto);
+
+        return "redirect:api/user/login-page";
+    }
+
+    @PostMapping("/user/login")
+    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
+
+        try {
+            userService.login(requestDto, res);
+        } catch (Exception e) {
+            return "redirect:/api/user/login-page?error";
+        }
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/sparta/icy/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.sparta.icy.repository;
+
+import com.sparta.icy.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User>  findByUsername(String username);
+    Optional<User>  findByEmail(String email);
+
+}
