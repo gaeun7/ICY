3e2cf245ceb29d51708d6d42cf305aa24f0b2134
Gaeun <rkdms1111111@naver.com>
2024-06-09 21:35:35 +0900

    컨트롤러 수정

--- src/main/java/com/sparta/icy/controller/NewsfeedController.java
+++ src/main/java/com/sparta/icy/controller/NewsfeedController.java
@@ -5,6 +5,7 @@ import com.sparta.icy.dto.NewsfeedResponseDto;
 import com.sparta.icy.service.NewsfeedService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class NewsfeedController {
         this.newsfeedService = newsfeedService;
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping
     public void createNewsfeed(@RequestBody NewsfeedDto newsfeedDto) {
         newsfeedService.createNewsfeed(newsfeedDto);
@@ -29,11 +31,13 @@ public class NewsfeedController {
         return newsfeedService.getNewsfeed(id);
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PutMapping("/{feed_id}")
     public void updateNewsfeed(@PathVariable Long feed_id, @RequestBody NewsfeedDto newsfeedDto) {
         newsfeedService.updateNewsfeed(feed_id, newsfeedDto);
     }
 
+    @PreAuthorize("isAuthenticated()")
     @DeleteMapping("/{feed_id}")
     public void deleteNewsfeed(@PathVariable Long feed_id) {
         newsfeedService.deleteNewsfeed(feed_id);
