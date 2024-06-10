be3a3ad61c731340ab496432bb821e52fe02f500
Gaeun <rkdms1111111@naver.com>
2024-06-10 22:21:46 +0900

    접근 관련 수정

--- src/main/java/com/sparta/icy/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/icy/config/WebSecurityConfig.java
@@ -75,7 +75,6 @@ public class WebSecurityConfig {
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // 리소스에 대한 접근 허용
                         .requestMatchers("/user/sign-up", "/log/**").permitAll() //
                         .requestMatchers("/newsfeed").permitAll() // 모든 사용자에게 뉴스피드 조회 허용
-                        .requestMatchers("/newsfeed/*").permitAll() // 모든 사용자에게 특정 게시물 조회 허용
                         .requestMatchers("/newsfeed/create").authenticated() // 게시물 작성, 수정, 삭제는 인증 필요
                         .requestMatchers("/user/sign-out").authenticated()
                         .anyRequest().authenticated() // 나머지 요청은 인증 필요
